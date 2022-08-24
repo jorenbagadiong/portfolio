008d72ba041c6db52ae0a6f67a36c8108e1049a3
Joren Bagadiong <joren.bagadiong14@gmail.com>
2022-08-24 22:54:32 +0800

    download CV

--- components/Hero.js
+++ components/Hero.js
@@ -1,33 +1,67 @@
 import Image from "next/image"
-import Link from "next/link"
+import axios from "axios"
 
-import homeImg from "../public/assets/images/Home.svg"
-import Button from "./Button"
+import { Button } from "components"
 
 export default function Hero() {
+  const dowloadCV = () => {
+    axios({
+      url: "/file/CV.docx",
+      method: "GET",
+      responseType: "blob",
+    }).then((res) => {
+      const url = window.URL.createObjectURL(new Blob([res.data]))
+      const link = document.createElement("a")
+      link.href = url
+      link.setAttribute("download", "Joren Bagadiong.docx") //or any other extension
+      document.body.appendChild(link)
+      link.click()
+    })
+  }
+
   return (
     <section id="hero">
       <div className="container mx-auto">
-        <div className="flex align-center justify-between mx-auto">
+        <div className="flex flex-col lg:flex-row align-center justify-between item-center mx-auto">
           <div
-            className="my-auto ml-[30px]"
+            className="my-auto ml-[30px] flex-1"
             data-aos="fade-right"
             data-aos-offset="300"
           >
-            <h1 className="h1">Hi,</h1>
-            <h1 className="h1 mt-[-40px]">
-              I’m <span className="gradientText">Joren</span>
-            </h1>
-            <h3 className="h3 mt-[-20px]">Frontend Developer</h3>
-            <div className="flex mt-[30px] gap-x-[20px]">
-              <button type="button" className="gradientButton">
-                Download CV
-              </button>
-              <Button name="Contact Me" path="#contact" width={200} height={200} />
+            <div>
+              <h1 className="h1">Hi,</h1>
+              <h1 className="h1 mt-[-40px]">
+                I’m <span className="gradientText">Joren</span>
+              </h1>
+              <h3 className="h3 mt-[-20px]">Frontend Developer</h3>
+              <div className="flex mt-[30px] gap-x-[20px]">
+                <div>
+                  <button
+                    type="button"
+                    className="gradientButton"
+                    onClick={dowloadCV}
+                  >
+                    Download CV
+                  </button>
+                </div>
+                <div>
+                  <Button
+                    name="Contact Me"
+                    path="#contact"
+                    width={200}
+                    height={200}
+                  />
+                </div>
+              </div>
             </div>
           </div>
           <div data-aos="fade-left" data-aos-offset="300">
-            <Image alt="home-image" src={homeImg} width="700" height="700" />
+            <Image
+              alt="home-image"
+              src="/assets/images/Home.svg"
+              width="700"
+              height="700"
+            />
           </div>
         </div>
       </div>
